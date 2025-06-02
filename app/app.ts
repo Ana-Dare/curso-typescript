6a7253e980c4fec3995cbff002e39562c57dc2cd
Ana Laura Dare <analauradare467@gmail.com>
2025-06-02 09:44:55 -0300

    Criando classe Negociacaocontroller

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,8 @@
-import { Negociacao } from "./models/negociacao.js";
+import { Negociacaocontroller } from "./controllers/negociacao-controller.js";
 
-const negociacao: Negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new Negociacaocontroller();
+const form = document.querySelector('.form');
+form.addEventListener( 'submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
