b8e749b85ae76da93c84282e9cba458427843d30
Ana Laura Dare <analauradare467@gmail.com>
2025-06-05 09:45:34 -0300

    Decorators finalizado

--- app/decorators/logar-tempo-de-execucao.ts
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -5,12 +5,13 @@ export function logarTempoDeExecucao() {
         descriptor: PropertyDescriptor
      ) {
         const metodoOriginal = descriptor.value;
-        descriptor.value = function() {
+        descriptor.value = function(...args: any[]) {
             const t1 = performance.now();
-            const retorno = metodoOriginal();
+            const retorno = metodoOriginal.apply(this,args);
             const t2 = performance.now();
             console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000}`);
-            retorno
+            return retorno;
         };
      }
 }
+
